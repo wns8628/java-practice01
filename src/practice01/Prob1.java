925a186840e1501c9aa1a0e9f4e9feee01889c3e
wns8628 <wns8628>
2018-12-27 08:45:16 +0900

    1번완료

--- src/practice01/Prob1.java
+++ src/practice01/Prob1.java
@@ -6,6 +6,18 @@ public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		
+		System.out.println("수를 입력하세요");
+		
+		int num;
+		
+		num = scanner.nextInt();
+		
+		if((num % 3) == 0 && num >= 3) {
+			System.out.println("3의배수입니다.");	
+		}else {
+			System.out.println("3의 배수가아닙니다.");
+		}
+		
 		
 		scanner.close();
 	}
