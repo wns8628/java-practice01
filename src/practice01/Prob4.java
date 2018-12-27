6d8e172146a65d999fb2faff1ecd81adc948e06d
wns8628 <wns8628>
2018-12-27 12:29:58 +0900

    4번끝

--- src/practice01/Prob4.java
+++ src/practice01/Prob4.java
@@ -1,9 +1,24 @@
 package practice01;
-
+import java.util.Scanner;
 
 public class Prob4 {
-
 	public static void main(String[] args) {
-	}
+		Scanner scanner = new Scanner( System.in );
+		
+		String message;
+		String[] strArray;
+		
+        System.out.print("문자열을 입력하세요: ");
+        message = scanner.nextLine();
+        strArray = message.split("");
 
+  
+        
+        for(int i =0; i< strArray.length; i++) {
+	        for(int j = 0; j<=i; j++) {
+	        	    System.out.print(strArray[j]);        			
+	        }
+	        System.out.println("");
+        }
+	}
 }
