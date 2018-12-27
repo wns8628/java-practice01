708ba2fe30c47c50ea98d9503046b3e03ada778a
wns8628 <wns8628>
2018-12-27 09:14:20 +0900

    2번끝

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -2,17 +2,26 @@ package practice01;
 
 public class Prob2 {
 	public static void main(String[] args) {
-		int a = 1;
-		int j = 10;
+
+//		int a = 1;
+//		int j = 10;
+//		while(j<=18) {
+//		 for(int i=a; i<=j; i++) {	
+//		  System.out.print(i+" ");
+//		 }
+//		 System.out.println(""); //줄바꿈
+//		 a++;
+//		 j++;
+//		}
 		
-		while(j<=18) {
-		 for(int i=a; i<=j; i++) {	
-		  System.out.print(i+" ");
-		 }
-		 System.out.println("");
-		 a++;
-		 j++;
+		//for문으로만이 젤간단
+		for(int j=10; j<=18; j++) {
+			for(int i =1; i<=j; i++) {
+				System.out.print(i+" ");
+			}
+			System.out.println("");
 		}
+	
 	}
 }
 
