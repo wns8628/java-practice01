8e0af95faeaaddbeff6f5ea4992c9d530885a142
wns8628 <wns8628>
2018-12-27 09:03:08 +0900

    2번 완료

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -2,5 +2,17 @@ package practice01;
 
 public class Prob2 {
 	public static void main(String[] args) {
+		int a = 1;
+		int j = 10;
+		
+		while(j<=18) {
+		 for(int i=a; i<=j; i++) {	
+		  System.out.print(i+" ");
+		 }
+		 System.out.println("");
+		 a++;
+		 j++;
+		}
 	}
 }
+
