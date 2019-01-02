48a0c97dcbef6a648cd5f90531ed7350a8591a86
maimai <maimaitoon@inbox.lv>
2019-01-02 18:58:05 +0900

    응?

--- src/practice01/Prob1.java
+++ src/practice01/Prob1.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class Prob1 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
-		
+		Scanner scanner = new Scanner( System.in );		
 		System.out.println("수를 입력하세요");
 		
 		int num;
